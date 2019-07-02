760cfa4c21b4620a56587b8f340decd78a5485ad
Zeljko Ilic <zeljkoilic84@gmail.com>
2019-07-02 20:30:01 +0200

    New components

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
+
+const Burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(ingredientKey => {
+            return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+                return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />;
+            });
+        }).reduce((arr, el) => arr.concat(el), []); 
+    if (transformedIngredients.length === 0) {
+         transformedIngredients = <p>Please start adding ingredients</p>
+    }
+    return (
+        <div className={classes.Burger}>
+             <BurgerIngredient type='bread-top' />
+             {transformedIngredients}
+             <BurgerIngredient type='bread-bottom' />
+        </div>
+    );
+};
+
+export default Burger;
